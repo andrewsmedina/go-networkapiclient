c45821ad30ead006d9557b0fe00602d416bf2ace
Andrews Medina <andrewsmedina@gmail.com>
2016-07-07 14:09:23 -0300

    add method to list vlans

--- client.go
+++ client.go
@@ -1,15 +1,21 @@
 package network
 
 import (
+	"encoding/json"
+	"errors"
+	"fmt"
 	"net"
 	"net/http"
 	"net/url"
+	"strings"
 	"time"
 
 	"github.com/hashicorp/go-cleanhttp"
 )
 
-// Client is the basic type of this package. It provides methods for
+const userAgent = "go-networkapiclient"
+
+// Client is the basic type of this pacGkage. It provides methods for
 // interaction with the API.
 type Client struct {
 	HTTPClient *http.Client
@@ -35,4 +41,49 @@ func NewClient(endpoint string) (*Client, error) {
 		endpoint:   u,
 	}
 	return client, nil
+}
+
+type Vlan struct{}
+type ListVlansOptions struct{}
+
+func (c *Client) getURL(path string) string {
+	urlStr := strings.TrimRight(c.endpoint.String(), "/")
+	return fmt.Sprintf("%s%s", urlStr, path)
+}
+
+func (c *Client) do(method, path string) (*http.Response, error) {
+	httpClient := c.HTTPClient
+	var u string
+	u = c.getURL(path)
+	// If the user has provided a timeout, apply it.
+	if c.timeout != 0 {
+		httpClient.Timeout = c.timeout
+	}
+	req, err := http.NewRequest(method, u, nil)
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Set("User-Agent", userAgent)
+	resp, err := httpClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	if resp.StatusCode < 200 || resp.StatusCode >= 400 {
+		return nil, errors.New("error")
+	}
+	return resp, nil
+}
+
+func (c *Client) ListVlans(opts ListVlansOptions) ([]Vlan, error) {
+	path := "/vlan/find"
+	resp, err := c.do("GET", path)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	var vlans []Vlan
+	if err := json.NewDecoder(resp.Body).Decode(&vlans); err != nil {
+		return nil, err
+	}
+	return vlans, nil
 }
--- client_test.go
+++ client_test.go
@@ -1,6 +1,11 @@
 package network
 
 import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"reflect"
 	"testing"
 )
 
@@ -12,5 +17,35 @@ func TestNewClient(t *testing.T) {
 	}
 	if client.endpoint.String() != endpoint {
 		t.Errorf("Expected endpoint %s. Got %s.", endpoint, client.endpoint)
+	}
+}
+
+func TestListVlans(t *testing.T) {
+	jsonVlans := `[
+     {
+             "Id": "8dfafdbc3a40",
+             "Image": "base:latest",
+             "Command": "echo 1",
+             "Created": 1367854155,
+             "Ports":[{"PrivatePort": 2222, "PublicPort": 3333, "Type": "tcp"}],
+             "Status": "Exit 0"
+     }
+]`
+	var expected []Vlan
+	err := json.Unmarshal([]byte(jsonVlans), &expected)
+	if err != nil {
+		t.Fatal(err)
+	}
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, jsonVlans)
+	}))
+	defer ts.Close()
+	client, err := NewClient(ts.URL)
+	vlans, err := client.ListVlans(ListVlansOptions{})
+	if err != nil {
+		t.Fatal(err)
+	}
+	if !reflect.DeepEqual(vlans, expected) {
+		t.Errorf("ListVlans: Expected %#v. Got %#v.", expected, vlans)
 	}
 }
